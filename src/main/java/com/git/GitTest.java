e12c4c9b7991195a4f48a7e58fc38bd51d50b6e4
weiqingbin666 <1021577519@qq.com>
2021-10-16 22:26:57 +0800

    hotfix commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -3,6 +3,6 @@ package com.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello1 git i am weqb");
-        System.out.println("second update");
+        System.out.println("second update ");
     }
 }
