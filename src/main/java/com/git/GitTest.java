90757d3f631d402c8fa4cccadffd953f88677484
weiqingbin666 <1021577519@qq.com>
2021-10-16 22:12:06 +0800

    second commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -2,6 +2,7 @@ package com.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git i am weiqb");
+        System.out.println("hello1 git i am weqb");
+        System.out.println("second update");
     }
 }
