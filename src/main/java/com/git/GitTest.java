92e40201333861e37b61194a615ae3cc3c1968c9
weiqingbin666 <1021577519@qq.com>
2021-10-17 09:32:28 +0800

    master commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -4,6 +4,5 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello1 git i am weqb");
         System.out.println("hotfix second");
-        System.out.println("master commit");
     }
 }
