f5d88857cf54e2aec1d87cedb59909cd63555ba7
QingBinWei <55903793+QingBinWei@users.noreply.github.com>
2021-10-17 10:06:44 +0800

    Update GitTest.java

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -3,7 +3,7 @@ package com.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello1 git i am weqb");
-        System.out.println("hotfix cond333");
+        System.out.println("hotfix");
         System.out.println("fffffffffffff");
         System.out.println("提交到远程github");
         System.out.println("github修改");
