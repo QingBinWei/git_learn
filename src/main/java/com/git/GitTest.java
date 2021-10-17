3babc5cd7add966da9778b74c1c53feffaa8f17d
QingBinWei <55903793+QingBinWei@users.noreply.github.com>
2021-10-17 10:02:14 +0800

    Update GitTest.java
    呃呃呃

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hotfix cond333");
         System.out.println("fffffffffffff");
         System.out.println("提交到远程github");
+        System.out.println("github修改");
     }
 }
