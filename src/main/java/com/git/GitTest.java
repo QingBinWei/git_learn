9403970f3c0676b12c59cd3c1649010c4e0815e4
weiqingbin666 <1021577519@qq.com>
2021-10-17 09:58:47 +0800

    hotfix commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello1 git i am weqb");
         System.out.println("hotfix cond333");
         System.out.println("fffffffffffff");
+        System.out.println("master 提交到远程github");
     }
 }
